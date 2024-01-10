2b737b50605364436bb5f04570912d87b203acd7
Hannes Wendt <hanneswendt22@gmail.com>
2024-01-10 16:17:07 +0100

    Add profiler

--- flake.nix
+++ flake.nix
@@ -4,6 +4,16 @@
       let
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python311;
+
+        flameprof = pkgs.python311Packages.buildPythonPackage rec {
+          pname = "flameprof";
+          version = "0.4";
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            hash = "sha256-28htQZDLu6Yk8eCkD0TZ25YTjidTTYPI70LUIIV4daM=";
+          };
+        };
+
         pyenv = python.withPackages (ps: with ps; [
           matplotlib
           numpy
@@ -15,6 +25,7 @@
           packages = with pkgs; [
             pyenv
             python311Packages.ipython
+            flameprof
           ];
         };
       });
